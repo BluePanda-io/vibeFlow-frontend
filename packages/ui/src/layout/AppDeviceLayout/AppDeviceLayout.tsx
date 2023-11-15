55c5f5416319f890c117e820ef329da99b44a19d
Miltiadis Saratzidis <miltossaratzidis1@gmail.com>
2023-11-15 13:41:46 -0500

    fix mobile

--- packages/ui/src/layout/AppDeviceLayout/AppDeviceLayout.tsx
+++ packages/ui/src/layout/AppDeviceLayout/AppDeviceLayout.tsx
@@ -14,25 +14,25 @@ export const AppDeviceLayout = ({ children }: IAppDeviceLayoutProps) => {
     )
   );
 
-  if (isMobile) {
-    return (
-      <>
-        {isMobile && (
-          <section className="fixed top-0 left-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-white">
-            <h1 className="text-edenGreen-600">Eden</h1>
-            <p className="mb-2 text-center">
-              Mobile site is
-              <br /> under construction.
-              <br /> Use your laptop.
-            </p>
-            <MdMobileOff className="text-2xl" />
-          </section>
-        )}
-      </>
-    );
-  } else {
-    return children;
-  }
+  // if (isMobile) {
+  //   return (
+  //     <>
+  //       {isMobile && (
+  //         <section className="fixed top-0 left-0 z-50 flex h-screen w-screen flex-col items-center justify-center bg-white">
+  //           <h1 className="text-edenGreen-600">Eden</h1>
+  //           <p className="mb-2 text-center">
+  //             Mobile site is
+  //             <br /> under construction.
+  //             <br /> Use your laptop.
+  //           </p>
+  //           <MdMobileOff className="text-2xl" />
+  //         </section>
+  //       )}
+  //     </>
+  //   );
+  // } else {
+  return children;
+  // }
 };
 
 export default AppDeviceLayout;
