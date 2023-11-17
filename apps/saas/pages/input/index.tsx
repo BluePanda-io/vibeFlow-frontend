8470b8fdab88eca1be874abf46e8de4bb69492dd
Miltiadis Saratzidis <miltossaratzidis1@gmail.com>
2023-11-17 18:16:58 -0500

    freat/fixLogin3

--- apps/saas/pages/input/index.tsx
+++ apps/saas/pages/input/index.tsx
@@ -1,6 +1,7 @@
 import { gql, useMutation } from "@apollo/client";
 import { UserContext } from "@eden/package-context";
-import { AppUserLayout, SaasUserLayout } from "@eden/package-ui";
+import { AppUserLayout, Button, SaasUserLayout } from "@eden/package-ui";
+import { signIn } from "next-auth/react";
 import React, { useContext, useState } from "react";
 
 import type { NextPageWithLayout } from "../_app";
@@ -64,90 +65,102 @@ const ConnectTGPage: NextPageWithLayout = () => {
   return (
     <>
       <AppUserLayout>
-        <form>
-          <div className="grid gap-6 mb-6 md:grid-cols-2 p-6">
-            <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-black">
-              Flow of the Day
-            </label>
-
-            <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
-                Energy Level: {energy} -
-              </label>
-              <div
-                style={{
-                  height: "50px",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <input
-                  type="range"
-                  id="large-range"
-                  // className="bg-gray-50 border border-gray-300 text-gray-500 text-2xl rounded-lg cursor-pointer range-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  className=" w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-300"
-                  min="1"
-                  max="10"
-                  placeholder="5"
-                  required
-                  value={energy}
-                  onChange={(e) => {
-                    setEnergy(e.target.value);
-                    navigator.vibrate(15); // Haptic feedback for mobile devices
-                  }}
-                ></input>
-              </div>
-            </div>
-            <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
-                Happiness Level {happiness}
-              </label>
-              <input
-                type="range"
-                min="1"
-                max="10"
-                id="last_name"
-                // className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                className=" w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-300"
-                placeholder="5"
-                required
-                value={happiness}
-                onChange={(e) => setHappiness(e.target.value)}
-              ></input>
-            </div>
-            <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
-                Stress Level {stress}
-              </label>
-              <input
-                type="range"
-                min="1"
-                max="10"
-                id="last_name"
-                // className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                className=" w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-300"
-                placeholder="5"
-                required
-                value={stress}
-                onChange={(e) => setStress(e.target.value)}
-              ></input>
-            </div>
-            <div style={{ padding: "10px" }}></div>
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        {currentUser?._id === undefined ? (
+          <div
+            className="col-span-4 flex items-center justify-center"
+            style={{ marginTop: "50px" }}
+          >
+            <Button
               onClick={() => {
-                saveToBackend();
-                alert("Done ✅");
-                setEnergy("");
-                setStress("");
-                setHappiness("");
+                signIn("google", { callbackUrl: "/input" });
               }}
             >
-              Submit
-            </button>
+              Log in with Google
+            </Button>
           </div>
-        </form>
+        ) : (
+          <form>
+            <div className="grid gap-6 mb-6 md:grid-cols-2 p-6">
+              <label className="block mb-2 text-xl font-medium text-gray-900 dark:text-black">
+                Flow of the Day
+              </label>
+
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                  Energy Level: {energy} -
+                </label>
+                <div
+                  style={{
+                    height: "50px",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <input
+                    type="range"
+                    id="large-range"
+                    className=" w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-300"
+                    min="1"
+                    max="10"
+                    placeholder="5"
+                    required
+                    value={energy}
+                    onChange={(e) => {
+                      setEnergy(e.target.value);
+                      navigator.vibrate(15); // Haptic feedback for mobile devices
+                    }}
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                  Happiness Level {happiness}
+                </label>
+                <input
+                  type="range"
+                  min="1"
+                  max="10"
+                  id="last_name"
+                  className=" w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-300"
+                  placeholder="5"
+                  required
+                  value={happiness}
+                  onChange={(e) => setHappiness(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                  Stress Level {stress}
+                </label>
+                <input
+                  type="range"
+                  min="1"
+                  max="10"
+                  id="last_name"
+                  className=" w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-300"
+                  placeholder="5"
+                  required
+                  value={stress}
+                  onChange={(e) => setStress(e.target.value)}
+                ></input>
+              </div>
+              <div style={{ padding: "10px" }}></div>
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={() => {
+                  saveToBackend();
+                  alert("Done ✅");
+                  setEnergy("");
+                  setStress("");
+                  setHappiness("");
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
       </AppUserLayout>
     </>
   );
